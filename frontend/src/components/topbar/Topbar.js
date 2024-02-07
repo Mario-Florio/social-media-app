e718c963a4e56a4855da5450c639b12bbbc409cc
ketherball <121590283+ketherball@users.noreply.github.com>
2024-02-07 08:34:51 -0500

    feat: create basic functioning search dropdown

--- frontend/src/components/topbar/Topbar.js
+++ frontend/src/components/topbar/Topbar.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./topbar.css";
 import searchIcon from "../../assets/imgs/search-icon.png";
+
+import getData from "../../dummyData";
 
 function Topbar({ sideMenuIsActive, setSideMenuIsActive }) {
     return(
@@ -33,33 +36,81 @@ function HamburgerMenu({ sideMenuIsActive, setSideMenuIsActive }) {
 
 function SearchBar() {
     const [isFocused, setIsFocused] = useState(false);
+    const [input, setInput] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        const suggestions = searchData(input);
+        setSuggestions(suggestions);
+
+        function searchData(input) {
+            const { users } = getData();
+
+            const results = [];
+            users.forEach(user => {
+                if (user.username.toLowerCase().includes(input.toLowerCase())) {
+                    results.push(user);
+                }
+            });
+            
+            return results;
+        }
+    }, [input]);
 
     function handleFocus() {
         setIsFocused(true);
     }
 
-    function handleBlur() {
-        setIsFocused(false);
+    function handleBlur(e) {
+        setTimeout(() => {
+            setIsFocused(false);
+        }, 130);
+    }
+
+    function handleChange(e) {
+        setInput(e.target.value);
     }
 
     function handleSubmit(e) {
         e.preventDefault();
+        setInput("");
     }
 
     return(
         <form onSubmit={handleSubmit} className={ isFocused ? "focused" : "" }>
-            <label htmlFor="search" className="hide">Search</label>
-            <button>
-                <img src={searchIcon} alt="Search"/>
-            </button>
-            <input
-                type="text"
-                name="search"
-                id="search"
-                placeholder="Search something..."
-                onFocus={handleFocus}
-                onBlur={handleBlur}
-            />
+            <div className="dropdown">
+                <div className="dropdown_top">
+                    <label htmlFor="search" className="hide">Search</label>
+                    <button>
+                        <img src={searchIcon} alt="Search"/>
+                    </button>
+                    <input
+                        type="text"
+                        name="search"
+                        id="search"
+                        placeholder="Search something..."
+                        value={input}
+                        onChange={handleChange}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                        autoComplete="off"
+                    />
+                </div>
+                <ul className={ isFocused ? "dropdown_bottom active" : "dropdown_bottom" }>
+                    { input.length > 0 && suggestions.map(user =>
+                        <li key={user._id}>
+                            <Link
+                                to={`/profile/${user.profile._id}`}
+                                onClick={() => setInput("")}
+                            >
+                                <div className="profile-pic_wrapper">
+                                    <img src={user.profile.pic} alt="user profile pic"/>
+                                </div>
+                                <h5>{user.username}</h5>
+                            </Link>
+                        </li>) }
+                </ul>
+            </div>
         </form>
     );
 }
