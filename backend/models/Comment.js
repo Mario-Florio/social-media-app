ab0cf42ebf2f8ef8dcce3d89fdf3544e12d7f110
ketherball <121590283+ketherball@users.noreply.github.com>
2024-02-15 12:03:53 -0500

    revert model changes

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
     text: { type: String, required: true, minLength: 3, maxLength: 250 }
 }, { timestamps: true });
 
--- backend/models/Post.js
+++ backend/models/Post.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    text: { type: String, required: true, minLength: 3, maxLength: 250 }
+    text: { type: String, required: true, minLength: 3, maxLength: 250 },
+    comments: { type: [Schema.Types.ObjectId], ref: "Comment", default: [] }
 }, { timestamps: true });
 
 const Post = mongoose.model("Post", PostSchema);
