af863621d813ac6ac89e4b4532ea379966053cb5
ketherball <121590283+ketherball@users.noreply.github.com>
2024-02-16 11:14:11 -0500

    feat: add likes field

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     text: { type: String, required: true, minLength: 3, maxLength: 250 },
+    likes: { type: [Schema.Types.ObjectId], ref: "Users", default: [] },
     comments: { type: [Schema.Types.ObjectId], ref: "Comment", default: [] }
 }, { timestamps: true });
 
