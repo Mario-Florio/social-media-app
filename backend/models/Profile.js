626d9048d9f17a2cf8ff75cd7d97887ea4a0d9c5
ketherball <121590283+ketherball@users.noreply.github.com>
2024-02-16 09:52:52 -0500

    feat: add default profile and cover pic

--- backend/models/Profile.js
+++ backend/models/Profile.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 
 const ProfileSchema = new Schema({
     bio: { type: String, maxLength: 250, default: "" },
-    picture: { type: String, default: "" },
-    coverPicture: { type: String, default: "" },
+    picture: { type: String, default: "../../assets/imgs/default/profile-picture.jpg" },
+    coverPicture: { type: String, default: "../../assets/imgs/default/cover-photo.jpg" },
     forum: { type: Schema.Types.ObjectId, ref: "Forum", required: true },
     followers: { type: [Schema.Types.ObjectId], ref: "Profile", default: [] },
     following: { type: [Schema.Types.ObjectId], ref: "Profile", default: [] }
