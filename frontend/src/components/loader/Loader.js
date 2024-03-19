e5ebc350f0603e640377459794eadd69669eb71b
ketherball <121590283+ketherball@users.noreply.github.com>
2024-03-19 13:05:11 -0400

    fix: design issue where loaders colors would not display on entry form

--- frontend/src/components/loader/Loader.js
+++ frontend/src/components/loader/Loader.js
@@ -1,22 +1,24 @@
 import "./loader.css";
 
-function Loader({ color, secondaryColor, size }) {
-    const borderTopColor = { borderTopColor: color || "var(--target-color" };
-    const borderColor = { borderColor: secondaryColor || "var(--secondary-color" };
+function Loader({ color="dodgerblue", secondaryColor="lightgray", size=40 }) {
+    const borderTopColor = { borderTopColor: color };
+    const borderColor = { borderColor: secondaryColor };
     const heightByWidth = {
-        height: `${size}px` || "40px",
-        width: `${size}px` || "40px",
-        borderWidth: `${size/6.66666667}px` || "6px"
+        height: `${size}px`,
+        width: `${size}px`,
+        borderWidth: `${size/6.66666667}px`
+    }
+
+    const styles = {
+        ...borderColor,
+        ...borderTopColor,
+        ...heightByWidth
     }
     
     return(
         <div
             className="loader"
-            style={{
-                ...borderColor,
-                ...borderTopColor,
-                ...heightByWidth
-            }}
+            style={styles}
         ></div>
     );
 }
