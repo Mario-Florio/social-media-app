6198006667c82ed1c30714add7503af36108336e
ketherball <121590283+ketherball@users.noreply.github.com>
2024-03-20 06:57:58 -0400

    remove unused code

--- frontend/src/serverRequests/methods/users.js
+++ frontend/src/serverRequests/methods/users.js
@@ -18,37 +18,6 @@ async function populateUsers(userIds) {
     return populatedUsers;
 }
 
-// Profile
-async function populateProfileUser(profileId) {
-    const { users } = await getUsers();
-
-    let populatedUser = null;
-    users.forEach(user => {
-        if (user.profile._id.toString() === profileId) {
-            populatedUser = user;
-        }
-    });
-
-    return populatedUser;
-}
-
-// Topbar
-async function searchUsers(input) {
-    if (input.length === 0) return [];
-    const { users } = await getUsers();
-
-    const results = [];
-    users.forEach(user => {
-        if (user.username.toLowerCase().includes(input.toLowerCase().trim())) {
-            results.push(user);
-        }
-    });
-    
-    return results;
-}
-
 export {
-    populateUsers,
-    populateProfileUser,
-    searchUsers
+    populateUsers
 }
