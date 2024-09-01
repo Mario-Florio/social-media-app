57e0434749e441aa3b581f642dbb22af8653e4cf
ketherball <121590283+ketherball@users.noreply.github.com>
2024-09-01 13:00:24 -0400

    fix: add forward slash to base url to conform to vite config standards

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -12,7 +12,7 @@ axios.defaults.baseURL = import.meta.env.VITE_PROXY;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="social-media-app">
+    <BrowserRouter basename="/social-media-app">
         <ResponsePopupProvider>
             <AuthProvider>
                 <App />
